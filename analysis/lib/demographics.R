370e1144dcb70eb58c904ea7d628728d355958ef
Owen Morgan <owen.morgan95@gmail.com>
2022-12-16 21:07:02 -0500

    Add field x level interaction analyses (n = 672)

--- analysis/lib/demographics.R
+++ analysis/lib/demographics.R
@@ -48,6 +48,20 @@ demo_table <- aah_summary |>
 return(demo_table)
 }
 
+sample_demo_summary_table <- function(aah_summary) {
+demo_table <- aah_summary |>
+  group_by(sample) |> 
+  summarize(
+            N = n(),
+            `Age (years)` = report_mean_sd(age),
+            `Education (years)`= report_mean_sd(education),
+            `Sex (M/F/O)` = report_sex(sex),
+            EHI = report_mean_sd(ehi)
+  ) |> 
+  rename(Sample = sample)
+return(demo_table)
+}
+
 demo_country_table <- function(aah_summary){
   aah_summary |>
   group_by(country) |>
