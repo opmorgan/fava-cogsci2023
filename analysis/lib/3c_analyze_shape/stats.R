59975a8bb90cacd8ec3bd3ba28d4f53facf73b05
Owen Morgan <owen.morgan95@gmail.com>
2023-01-11 17:29:03 -0500

    Add shape analyses for continuous handedness

--- analysis/lib/3c_analyze_shape/stats.R
+++ analysis/lib/3c_analyze_shape/stats.R
@@ -193,7 +193,7 @@ emmtest_F_L_S <- function(FLS_emm, data_label, title = "") {
 }
 
 
-## FIELD BY LEVEL BY SHAPE BY HANDEDNESS
+## FIELD BY LEVEL BY SHAPE BY HANDEDNESS (BINNED)
 model_emm_FLSH <- function(manual_cache_dir,
                           data_label,
                           data,
@@ -274,7 +274,7 @@ anova_FLSH <- function(use_cached_anova = FALSE,
 }
 
 
-emmtest_FLSH <- function(FLSH_emm, title = "title") {
+emmtest_FLSH <- function(FLSH_emm, title = "") {
   FLSH_emmtest <- FLSH_emm |>
     contrast(interaction = c("consec")) |>
     summary(infer = T)
@@ -377,6 +377,271 @@ emmtest_FL_H_S <- function(use_cached_model = FALSE,
                subtitle = "Compare effect estimate to zero with emmeans()") |>
     tab_footnote(footnote = "A positive number means LVF global bias",
                  locations = cells_column_labels(columns = estimate)) |>
+    tab_footnote(footnote = "Two-sided",
+                 locations = cells_column_labels(columns = p.value)) |>
+    tab_footnote(footnote = "Confidence level: 95%",
+                 locations = cells_column_labels(columns = ends_with("CL"))) |>
+    tab_footnote(footnote = "Z-approximation",
+                 locations = cells_column_labels(columns = df))
+  
+}
+
+
+
+## FIELD BY LEVEL BY SHAPE BY HANDEDNESS (continuous)
+model_emm_FLSehi <- function(manual_cache_dir,
+                           data_label,
+                           data,
+                           use_cached_model = FALSE) {
+  model_path <-
+    here(manual_cache_dir, str_c("FLSehi_model_", data_label, ".rds"))
+  emm_path <-
+    here(manual_cache_dir, str_c("FLSehi_emm_", data_label, ".rds"))
+  
+  if (use_cached_model == FALSE) {
+    ## Create model & emmeans object
+    model <- lmer(rt ~ field * level * ehi * shape + (1 | subject),
+                  data = data)
+    emm <- emmeans(model, ~ field * level * ehi * shape)
+    
+    ## Manually cache model
+    cli_progress_step(msg = "Caching model: {model_path}",
+                      msg_done = "Created and cached model: {model_path}")
+    saveRDS(model, model_path)
+    cli_progress_step(msg = "Caching emm object: {emm_path}",
+                      msg_done = "Created and cached emm_object: {emm_path}")
+    saveRDS(emm, emm_path)
+    
+  } else if (use_cached_model == TRUE) {
+    ## Load cached model & emmeans object
+    cli_progress_step(msg = "Loading model: {model_path}",
+                      msg_done = "Loaded model: {model_path}")
+    model <- readRDS(model_path)
+    cli_progress_step(msg = "Loading emm object: {emm_path}",
+                      msg_done = "Loaded emm object: {emm_path}")
+    emm <- readRDS(emm_path)
+  }
+  
+  model_emm <- list()
+  model_emm$model <- model
+  model_emm$emm <- emm
+  
+  return(model_emm)
+}
+
+anova_FLSehi <- function(use_cached_anova = FALSE,
+                       manual_cache_dir,
+                       FLSehi_model,
+                       data_label = data_label,
+                       title = "") {
+  anova_path <-
+    here(manual_cache_dir, str_c("FLSehi_anova_", data_label, ".rds"))
+  
+  if (use_cached_anova == FALSE) {
+    FLSehi_model_no_interaction <-
+      update(FLSehi_model, . ~ . - field:level:ehi:shape)
+    interaction_anova <-
+      interaction_stats(FLSehi_model, FLSehi_model_no_interaction)
+    
+    ## Cache anova
+    cli_progress_step(msg = "Caching anova: {anova_path}",
+                      msg_done = "Created and cached anova: {anova_path}")
+    saveRDS(interaction_anova, anova_path)
+    
+  } else if (use_cached_anova == TRUE) {
+    ## Load cached anova
+    cli_progress_step(msg = "Loading anova: {anova_path}",
+                      msg_done = "Loaded anova: {anova_path}")
+    interaction_anova <- readRDS(anova_path)
+  }
+  
+  cli_progress_step(msg = "Printing anova table: {anova_path}",
+                    msg_done = "Printed anova table: {anova_path}")
+  interaction_anova |>
+    as_tibble() |>
+    rename(p.value = `Pr(>Chisq)`) |>
+    format_p.value() |>
+    pretty_table() |>
+    tab_header(title = title,
+               subtitle = "ANOVA: compare models with vs. without interaction term") |>
+    tab_footnote(footnote = "F-test (two-sided? https://daniellakens.blogspot.com/2016/04/one-sided-f-tests-and-halving-p-values.html)",
+                 locations = cells_column_labels(columns = p.value))
+}
+
+emmtest_FLSehi <- function(use_cached_model = FALSE,
+                           manual_cache_dir,
+                           FLSehi_model,
+                           data_label,
+                           title = "") {
+  
+  emt_path <-
+    here(manual_cache_dir, str_c("FLSehi_emt_", data_label, ".rds"))
+  
+  
+  if (use_cached_model == FALSE) {
+    ## This code estimates the effect of EHI on RT for each field*level*shape:
+    emt <-
+      emtrends(FLSehi_model, pairwise ~ field * level * shape, var = "ehi")
+    
+    ## Manually cache emtrends object
+    cli_progress_step(msg = "Caching emtrends object: {emt_path}",
+                      msg_done = "Created and cached emtrends object: {emt_path}")
+    saveRDS(emt, emt_path)
+    
+  } else if (use_cached_model == TRUE) {
+    ## Load cached emtrends object
+    cli_progress_step(msg = "Loading emtrends object: {emt_path}",
+                      msg_done = "Created and cached emtrends object: {emt_path}")
+    cli_progress_step(msg = "Loading emtrends object: {emt_path}",
+                      msg_done = "Loaded emtrends object: {emt_path}")
+    emt <- readRDS(emt_path)
+  }
+    
+  emt$emtrends |>
+    contrast(interaction = c("consec")) |>
+    summary(infer = T) |>
+    as_tibble() |>
+    format_p.value() |>
+    pretty_table() |>
+    tab_header(title = title,
+               subtitle = "Compare effect estimate to zero with emmeans()") |>
+    tab_footnote(footnote = "A positive number means the interaction of field by level by EHI is stronger for squares",
+                 locations = cells_column_labels(columns = estimate)) |>
+    tab_footnote(footnote = "Two-sided",
+                 locations = cells_column_labels(columns = p.value)) |>
+    tab_footnote(footnote = "Confidence level: 95%",
+                 locations = cells_column_labels(columns = ends_with("CL"))) |>
+    tab_footnote(footnote = "Z-approximation",
+                 locations = cells_column_labels(columns = df))
+  
+}
+  
+
+emmtest_FLehi_S <- function(use_cached_model = FALSE,
+                           manual_cache_dir,
+                           FLSehi_model,
+                           data_label,
+                           title = "") {
+  
+  emt_path <-
+    here(manual_cache_dir, str_c("FLehi_S_emt_", data_label, ".rds"))
+  
+  
+  if (use_cached_model == FALSE) {
+    emt <-
+      emtrends(FLSehi_model, pairwise ~ field * level |
+                 shape, var = "ehi")
+    
+    ## Manually cache emtrends object
+    cli_progress_step(msg = "Caching emtrends object: {emt_path}",
+                      msg_done = "Created and cached emtrends object: {emt_path}")
+    saveRDS(emt, emt_path)
+    
+  } else if (use_cached_model == TRUE) {
+    ## Load cached emtrends object
+    cli_progress_step(msg = "Loading emtrends object: {emt_path}",
+                      msg_done = "Created and cached emtrends object: {emt_path}")
+    cli_progress_step(msg = "Loading emtrends object: {emt_path}",
+                      msg_done = "Loaded emtrends object: {emt_path}")
+    emt <- readRDS(emt_path)
+  }
+  
+  
+  emt$emtrends |>
+    contrast(interaction = c("consec"), var = "ehi") |>
+    summary(infer = T) |>
+    as_tibble() |>
+    format_p.value() |>
+    pretty_table() |>
+    tab_header(title = title,
+               subtitle = "") |>
+    tab_footnote(footnote = "A positive number means more global bias for right handers, in ms per EHI unit (-100 to 100)",
+                 locations = cells_column_labels(columns = estimate)) |>
+    tab_footnote(footnote = "Two-sided",
+                 locations = cells_column_labels(columns = p.value)) |>
+    tab_footnote(footnote = "Confidence level: 95%",
+                 locations = cells_column_labels(columns = ends_with("CL"))) |>
+    tab_footnote(footnote = "Z-approximation",
+                 locations = cells_column_labels(columns = df))
+  
+}
+
+pull_ehi_FLS <- function(use_cached_model = F,
+                          manual_cache_dir,
+                          FLSehi_model,
+                          data_label,
+                          title = "") {
+  ref_path <-
+    here(manual_cache_dir, str_c("FLehi_S_ref_", data_label, ".rds"))
+  
+  
+  if (use_cached_model == FALSE) {
+    ref <- ref_grid(FLSehi_model, at = list(ehi = c(-100, 0, 100)))
+    
+    ## Manually cache refrends object
+    cli_progress_step(msg = "Caching ref object: {ref_path}",
+                      msg_done = "Created and cached ref object: {ref_path}")
+    saveRDS(ref, ref_path)
+    
+  } else if (use_cached_model == TRUE) {
+    ## Load cached refrends object
+    cli_progress_step(msg = "Loading ref object: {ref_path}",
+                      msg_done = "Created and cached ref object: {ref_path}")
+    cli_progress_step(msg = "Loading ref object: {ref_path}",
+                      msg_done = "Loaded ref object: {ref_path}")
+    ref <- readRDS(ref_path)
+  }
+  
+  ref |>
+    contrast(interaction = c("consec"), by = "ehi") |>
+    summary(infer = T, type = "response") |>
+    as_tibble() |>
+    format_p.value() |>
+    pretty_table() |>
+    tab_header(title = title) |>
+    tab_footnote(footnote = "Strong left hander: -100; Mixed hander: 0; Strong right hander: +100",
+                 locations = cells_column_labels(columns = ehi)) |>
+    tab_footnote(footnote = "Estimated LVF global bias (ms); a positive number means LVF global bias",
+                 locations = cells_column_labels(columns = estimate))
+}
+
+
+pull_ehi_FL_S <- function(use_cached_model = F,
+                          manual_cache_dir,
+                          FLSehi_model,
+                          data_label,
+                          title = "") {
+  
+  ref_path <-
+    here(manual_cache_dir, str_c("FLehi_S_ref_", data_label, ".rds"))
+  
+  
+  if (use_cached_model == FALSE) {
+ref <- ref_grid(FLSehi_model, at = list(ehi = c(-100, 0, 100)))
+    
+    ## Manually cache refrends object
+    cli_progress_step(msg = "Caching ref object: {ref_path}",
+                      msg_done = "Created and cached ref object: {ref_path}")
+    saveRDS(ref, ref_path)
+    
+  } else if (use_cached_model == TRUE) {
+    ## Load cached refrends object
+    cli_progress_step(msg = "Loading ref object: {ref_path}",
+                      msg_done = "Created and cached ref object: {ref_path}")
+    cli_progress_step(msg = "Loading ref object: {ref_path}",
+                      msg_done = "Loaded ref object: {ref_path}")
+    ref <- readRDS(ref_path)
+  }
+
+ref |> contrast(interaction = "consec", by = c("shape", "ehi")) |> 
+  summary(infer = T) |> 
+  as_tibble() |> 
+  arrange(shape, ehi) |> 
+  format_p.value() |> 
+  pretty_table() |> 
+  tab_header(title = title) |> 
+  tab_footnote(footnote = "Estimated LVF global bias (ms); a positive number means LVF global bias",
+               locations = cells_column_labels(columns = estimate)) |> 
     tab_footnote(footnote = "Two-sided",
                  locations = cells_column_labels(columns = p.value)) |>
     tab_footnote(footnote = "Confidence level: 95%",
